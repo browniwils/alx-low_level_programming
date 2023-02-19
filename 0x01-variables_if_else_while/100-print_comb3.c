1e98537f9434df0d9ebc325c076769e619051659
Browni Wilson <browniwils@gmail.com>
2023-02-19 14:19:50 -0800

    Fix 100-print_comb3.c\

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#inlcude <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i == 8 && j == 9) 
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
