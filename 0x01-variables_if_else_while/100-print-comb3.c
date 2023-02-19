59f31df08ad01d5033518258fe572fc8958dd605
Browni Wilson <browniwils@gmail.com>
2023-02-19 05:55:10 -0800

    added advance task 10

---
+++ 0x01-variables_if_else_while/100-print-comb3.c
@@ -0,0 +1,23 @@
+#inlcude <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+while (i < 90);
+{
+if (i < 10);
+{
+printf("%d", i);
+}
+else
+{
+printf("%d", i);
+}
+return (0);
+}
+}
