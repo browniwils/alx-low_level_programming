a1c42b08044326475ecca7d37b41b9846952f568
Browni Wilson <browniwils@gmail.com>
2023-02-21 20:59:21 -0800

    fix task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,11 @@
 
 /**
  * _isalpha checks and confirm an alphabet character
+ * @ch: checking alphabet
  * Return: 1 when a @ch is an alphabet otherwise 0
  */
 
 int _isalpha(int ch)
 {
-return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
 }
