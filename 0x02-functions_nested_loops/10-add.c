441446e0b1bc2855510c45276832c2a066206d1d
Browni Wilson <browniwils@gmail.com>
2023-02-21 21:39:37 -0800

    added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds up integers and return it
+ * @i: integer to be added
+ * @j: integer to be added
+ * Return: return sum of integers
+ */
+
+int add(int i, int j)
+{
+return (i + j);
+}
