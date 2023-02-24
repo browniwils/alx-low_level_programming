e3b710beaa2cabb7cbffbd57f577794a0481fc77
Browni Wilson <browniwils@gmail.com>
2023-02-24 06:20:35 -0800

    Added task 4 for 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,8 @@
 int _isupper(char c);
 int _isdigit(char c);
 int mul(int a, int b);
-void print_numbers();
+void print_numbers(void);
 int _putchar(char c);
+void print_most_numbers(void);
 
 #endif
