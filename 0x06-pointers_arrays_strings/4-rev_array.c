ef6fd9506ce0c6cf4bb0bb23a0cc0dd3d7edae48
Browni Wilson <browniwils@gmail.com>
2023-03-02 15:55:48 -0800

    Added task 4 for 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ * @a: array
+ * @n: total number of array items
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+	
+	if (n > 0)
+	{
+		j = n - 1;
+		for (i = 0; i < n / 2; i++)
+		{
+			t = a[i];
+			a[i] = a[j];
+			a[j] = t;
+		}
+	}
+}
