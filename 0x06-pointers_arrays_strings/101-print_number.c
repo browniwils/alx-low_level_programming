6b883152d793ab86b670533cca7b5762b4042b59
Browni Wilson <browniwils@gmail.com>
2023-03-03 11:22:42 -0800

    Added advance task 9 for 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - print number
+ * @n: number integer
+ * Return: Always 0
+ */
+
+void print_number(int n);
+{
+	unsigned int i;
+
+	i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -1 * n;
+	}
+
+	if (i / 10 =! 0)
+	{
+		print_number(i / 10);
+	}
+
+	_putchar((i % 10) + '0');
+}
