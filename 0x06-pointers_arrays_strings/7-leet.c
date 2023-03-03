d241f561380520470a7c71fcf26be8c0a7a97888
Browni Wilson <browniwils@gmail.com>
2023-03-03 02:23:01 -0800

    Added task 7 for 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.c"
+
+/**
+ * leet - encode string
+ * @s: string to be encoded
+ * Return: encoded string
+ */
+
+char *leet(char *s)
+{
+	char enc[5] = {'4', '3', '0', '7', '1'};
+	char code[5] = {'A', 'E', 'O', 'T', 'L'};
+	int dbUL = 32; /* difference between uppercase and lowercase */
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == code[j] || s[i] == code[j] + dbUL)
+			{
+				s[i] = enc[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *str1, char *str2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
+char *cap_string(char *);
+char *leet(char *s);
 
 
 #endif
