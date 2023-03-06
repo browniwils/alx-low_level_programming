0af7c13ab53e2685e4a14ab7cadaf796f56ac89a
Browni Wilson <browniwils@gmail.com>
2023-03-06 12:31:06 +0000

    Fix task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,24 +11,18 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i, j;
-	int l = 0;
 
 	for (i = 0; haystack[i] != 0; i++)
 	{
 		for (j = 0; needle[j] != 0; j++)
 		{
-			if (needle[j] == haystack[i + l])
+			if (needle[j] != haystack[i + j])
 			{
-				l++;
-			}
-			else
-			{
-				l = 0;
 				break;
 			}
 		}
-
-		if (l)
+		
+		if (needle[j] == 0)
 		{
 			return (&haystack[i]);
 		}
