780a70b67b7b9dbdf644dee58f66ec9646ce3a89
Browni Wilson <browniwils@gmail.com>
2023-03-06 17:02:43 +0000

    Added task 8 for 0x07

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum of 2 diagonals of a square matrix
+ * @a: square matrix
+ * @size: size of matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	unsigned int ld, rd;
+
+	ld = 0;
+	rd = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		ld += a[(size * i) + i];
+		rd += a[(size * (i + 1)) - (i + 1)];
+	}
+
+	printf("%d, ", ld);
+	printf("%d\n", rd);
+}
