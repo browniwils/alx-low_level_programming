3ad36bfc3ae19234f982e246cb48d58f6996ef9d
Browni Wilson <browniwils@gmail.com>
2023-03-10 14:14:38 +0000

    Added task 0 for 0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints its filename
@@ -7,14 +8,9 @@
  * Return: always 0 is success
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
