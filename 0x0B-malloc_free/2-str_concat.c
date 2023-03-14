1478ce041ae560ceb94581f59b1f45ded8b933f5
Browni Wilson <browniwils@gmail.com>
2023-03-14 14:07:28 +0000

    Fix task 2 again

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,51 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * str_concat - concantenate to string in a new array
+ * *str_concat - concantenate to string in a new array
  * @s1: string 1
  * @s2: string 2
  * Return: new array of string 1 and 2 otherwise NULL
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	int i, j = 0, s1len = 0, s2len = 0;
 	char *arr;
+	unsigned int i = 0, j = 0, s1len = 0, s2len = 0;
 
 	while (s1 && s1[s1len])
 	{
-		s1len++;
+		len1++;
 	}
 	while (s2 && s2[s2len])
 	{
-		s2len++;
+		len2++;
 	}
+
 	arr = malloc(sizeof(char) * (s1len + s2len + 1));
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
+
 	if (s1)
 	{
-		for (i = 0; i < s1len; i++)
+		while (i < s1len)
 		{
 			arr[i] = s1[i];
+			i++;
 		}
 	}
+
 	if (s2)
 	{
-		for (; i < (s1len + s2len); i++)
+		while (i < (s1len + s2len))
 		{
 			arr[i] = s2[j];
+			i++;
+			j++;
 		}
 	}
 	arr[i] = '\0';
-	return (arr);
+
+	return (s3);
 }
