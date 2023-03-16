4ecbc24eee5e9aae453e26d55e4c46c0fa78ce29
Browni Wilson <browniwils@gmail.com>
2023-03-16 22:08:36 +0000

    Added task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - get lenght of a string
+ * @str: string
+ * Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - joins two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of @s2 needed
+ * Return: new string of 1 and 2 pointer otherwise NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, strlen1, strlen2;
+	char *memory;
+
+	strlen1 = _strlen(s1);
+	strlen2 = _strlen(s2);
+	if (n > strlen2)
+	{
+		memory = malloc((sizeof(char) * (strlen1 + strlen2)) + 1);
+		if (memory == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < strlen1; i++)
+		{
+			memory[i] = s1[i];
+		}
+		for (i = 0; i < strlen2; i++)
+		{
+			memory[strlen1 + i] = s2[i];
+		}
+		memory[strlen1 + i] = '\0';
+	}
+	else
+	{
+		memory = malloc((sizeof(char) * (strlen1 + n)) + 1);
+		if (memory == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < strlen1; i++)
+		{
+			memory[i] = s1[i];
+		}
+		for (i = 0; i < n; i++)
+		{
+			memory[strlen1 + i] = s2[i];
+		}
+		memory[strlen1 + i] = '\0';
+	}
+	return (memory);
+}
