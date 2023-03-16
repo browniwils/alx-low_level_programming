54607f4e96b1e42b6c72a3b6f35d54d504c71dea
Browni Wilson <browniwils@gmail.com>
2023-03-16 23:07:15 +0000

    Fixed again task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -48,7 +48,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = strlen2;
 	}
 
-	memory = malloc((sizeof(char) * (strlen1 + n)) + 1);
+	memory = malloc(sizeof(char) * (strlen1 + n + 1));
 	if (memory == NULL)
 	{
 		return (NULL);
