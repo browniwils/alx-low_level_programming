1d93b40849ea33d525d1aedaac2eb7880fc26ed9
Browni Wilson <browniwils@gmail.com>
2023-03-17 13:14:43 +0000

    Fixed task 4 for 0x0C

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 
-	if (new_size == 0 && ptr == NULL)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
