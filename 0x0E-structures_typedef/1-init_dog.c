e21057ea68a336983d6fa1662ad918f2e3d79336
Browni Wilson <browniwils@gmail.com>
2023-03-21 02:03:17 +0000

    fix task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d == NULL)
 	{
 		d = malloc(sizeof(struct dog));
-		if (d != NULL)
-		{
-			d.name = name;
-			d.age = age;
-			d.owner = owner;
-		}
 	}
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
 }
