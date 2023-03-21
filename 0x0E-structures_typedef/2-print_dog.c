ab4fe43f8b07de8d0de7853fa586684648ff9a5c
Browni Wilson <browniwils@gmail.com>
2023-03-21 02:30:58 +0000

    Added task 2 for 0x0E

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints info of dog
+ * @d: dog pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	char *nil = "(nil)";
+
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = nil;
+	}
+	else if (d->owner == NULL)
+	{
+		d->owner = nil;
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
