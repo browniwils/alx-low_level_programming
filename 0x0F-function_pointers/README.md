60f2efe9ef5b02f7babb357775900161b54030c2
Browni Wilson <browniwils@gmail.com>
2023-03-22 20:54:44 +0000

    Initiated project 0x0F with header and README

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+## 0x0F-function_pointers
+This project focus on pointers to functions and how it works
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+void _putchar(char *s);
+
+
+#endif
