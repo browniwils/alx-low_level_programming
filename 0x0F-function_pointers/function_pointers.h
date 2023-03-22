e9d679b5d85d967005e63b9418a236aca06f9461
Browni Wilson <browniwils@gmail.com>
2023-03-22 21:54:46 +0000

    Fix task 0 again

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdlib.h>
 
-void _putchar(char *s);
+int _putchar(char *s);
+void print_name(char *name, void (*f)(char *));
 
 
 #endif
