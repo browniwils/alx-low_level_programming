340f26c6ffdd0713caea55712c14273884e257c2
Browni Wilson <browniwils@gmail.com>
2023-03-22 21:08:23 +0000

    Added task 0 for 0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to be printed
+ * @f: pointer to callback function to print name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+
+	(*f)(name);
+}
