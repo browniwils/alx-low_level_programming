f23292b6844739e25ef63c349a72b0ae22463407
Browni Wilson <browniwils@gmail.com>
2023-03-23 23:21:07 +0000

    Added task1 for 0x10

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers from supplied arguments
+ * @separator: string for to be used in between printed numbers
+ * @n: number of arguments supplied
+ * @...: arguments to be supplied
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list argv;
+	unsigned int i;
+
+	if (n == 0 || separator == NULL)
+	{
+		return;
+	}
+
+	va_start(argv, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(argv, int));
+		if (i != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(argv);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 
 int _putchar(char *s);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
 
 
 #endif
