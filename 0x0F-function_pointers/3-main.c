9a3d15ceee86a7999047fca0c6e86f43921731a5
Browni Wilson <browniwils@gmail.com>
2023-03-23 03:41:58 +0000

    Fix task 3 for 0x0F

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,22 +16,23 @@ int main(int argc, char *argv[])
 	int input_1, input_2;
 	char *error_message = "Error";
 
-	input_1 = atoi(argv[1]);
-	input_2 = atoi(argv[3]);
-
-	if (argc != 4 || input_1 == 0 || input_2 == 0)
+	if (argc != 4)
 	{
 		printf("%s\n", error_message);
 		exit(98);
 	}
 
 	_operate = get_op_func(argv[2]);
-	if (_operate == NULL)
+	if (_operate == NULL || argv[2][1])
 	{
 		printf("%s\n", error_message);
 		exit(99);
 	}
 
+	input_1 = atoi(argv[1]);
+	input_2 = atoi(argv[3]);
+
 	printf("%d\n", _operate(input_1, input_2));
+
 	return (0);
 }
