b44419af6338c185d53f9eca44e971de868b8e6d
Browni Wilson <browniwils@gmail.com>
2023-03-31 12:26:34 +0000

    Added task 1 for 0x12

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - count number of elements in a linked list
+ * @h: linked list
+ * Return: number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned num_element = 0;
+
+	while (h)
+	{
+		num_element++;
+		h = h->next;
+	}
+	return (num_element);
+}
