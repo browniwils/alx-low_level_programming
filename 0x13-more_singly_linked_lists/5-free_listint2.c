dc2cc52a1b60da0a82b41908ddcae331ae4b58b0
Browni Wilson <browniwils@gmail.com>
2023-04-03 18:50:29 +0000

    Added task 5 for 0x13

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 -  free up list's memories
+ * @head: pointer to list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_node;
+
+	while ((*head) != NULL)
+	{
+		temp_node = (*head)->next;
+		free((*head));
+		(*head) = temp_node;
+	}
+	(*head) = NULL;
+}
