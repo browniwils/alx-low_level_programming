b7c3102cc640b992a13fce2f691565aa2c948865
Browni Wilson <browniwils@gmail.com>
2023-04-09 19:38:46 +0000

    Added task 5 for 0x14

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - flip bits
+ * @n: first number
+ * @m: second number
+ * Return: number of bits that was fliped
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i;
+	int j;
+
+	i = n ^ m;
+	while (i != 0)
+	{
+		j++;
+		i &= (i - 1);
+	}
+	return (j);
+}
