7403e26e7b963db7e51d94a0f51794b05658b437
Browni Wilson <browniwils@gmail.com>
2023-09-13 22:28:54 +0000

    added advanced task 100 for 0x1E

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - searches for a value in an array
+ * @array: array
+ * @size: size of array
+ * @value: value to search
+ * Return: index of the number
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int i, m, k, p;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	p = i = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+		k++;
+		p = i;
+		i = k * m;
+	} while (i < (int)size && array[i] < value);
+
+	printf("Value found between ies [%d] and [%d]\n", p, i);
+
+	for (; p <= i && p < (int)size; p++)
+	{
+		printf("Value checked array[%d] = [%d]\n", p, array[p]);
+		if (array[p] == value)
+			return (p);
+	}
+
+	return (-1);
+}
