bb42360d601a7279db4f8575d2fe2096edc90656
Browni Wilson <browniwils@gmail.com>
2023-09-13 22:10:35 +0000

    added task 0 for 0x1E

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - searches for a value in an arrray of integers
+  * @array: array
+  * @size: size of array
+  * @value: item to search
+  * Return: index of value found in array otherwise -1
+  */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (-1);
+		}
+	}
+
+	return (-1);
+}
